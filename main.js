5c5f1f44ff5010737cc96ac74099889b11f5954e
Radu Vasile <radu.vasile@mavenhut.com>
2014-11-22 14:30:07 +0200

    snake movement and collision with ball

--- main.js
+++ main.js
@@ -1,40 +1,17 @@
 var game;
+var snakeHead; //head of snake sprite
+var snakeSection = new Array(); //array of sprites that make the snake body sections
+var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
+var numSnakeSections = 4; //number of snake body sections
+var snakeSpacer = 14; //parameter that sets the spacing between sections
+var cursors;
+var bodyMover;
+var isPaused = false;
+var pauseButton;
+var storedVelocity = {x: 0, y: 0};
+var ball;
+
 window.onload = function() {
-
-//	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create }, true);
-//
-//	function preload () {
-//
-//		//game.load.image('logo', 'phaser.png');
-//
-//	}
-//
-//	function create() {
-//
-////		var button = game.add.button(game.world.centerX, game.world.centerY, onPlayAction, this);
-//
-//		var shape = game.add.graphics(0, 0);
-//
-//		shape.lineStyle(2, 0x00FF00, 1);
-//		shape.beginFill(0x345555, 1);
-//
-//		shape.drawRect(100,100,100,100);
-//
-//
-//		var playText = game.add.text(100, 100, 'Play', { font: "65px Arial", fill: "#ff0044", align: "center" });
-//
-//		if (Phaser.Keyboard(this).isDown(Phaser.Keyboard.LEFT)) {
-//			shape.ani
-//		}
-//	}
-//
-//
-//	function onPlayAction() {
-//		console.log('button is pressed')
-//	}
-
-
-	// Twitter: @pato_reilly Web: http://patricko.byethost9.com
 
 	game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
@@ -44,66 +21,91 @@ window.onload = function() {
 
 	}
 
-	var snakeHead; //head of snake sprite
-	var snakeSection = new Array(); //array of sprites that make the snake body sections
-	var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
-	var numSnakeSections = 4; //number of snake body sections
-	var snakeSpacer = 14; //parameter that sets the spacing between sections
-	var cursors;
-	var bodyMover;
-	var isPaused = false;
-	var pauseButton;
-	var storedVelocity = {x: 0, y: 0};
-
 	function create() {
 
+		// setup game physics
 		game.physics.startSystem(Phaser.Physics.P2JS);
+		game.physics.p2.restitution = 0.8;
+		game.physics.p2.setImpactEvents(true);
 
-		cursors = game.input.keyboard.createCursorKeys();
+		// setup collision groups
+		var snakeCollisionGroup = game.physics.p2.createCollisionGroup();
+		var ballCollisionGroup = game.physics.p2.createCollisionGroup();
+		game.physics.p2.updateBoundsCollisionGroup();
 
-		snakeHead = game.add.sprite(400, 300, 'ball');
+		// add the ball
+		ball = game.add.sprite(400, 300, 'ball');
+		ball.anchor.setTo(0.5, 0.5);
+		game.physics.p2.enable(ball);
+		ball.body.setCollisionGroup(ballCollisionGroup);
+		ball.body.collides([snakeCollisionGroup]);
+
+		// add the snake head
+		snakeHead = game.add.sprite(100, 300, 'ball');
 		snakeHead.anchor.setTo(0.5, 0.5);
-
 		game.physics.p2.enable(snakeHead);
 
-		//  Init snakeSection array
-		for (var i = 1; i <= numSnakeSections-1; i++)
+		snakeHead.body.fixedRotation = true;
+		snakeHead.body.setCollisionGroup(snakeCollisionGroup);
+		snakeHead.body.collides(ballCollisionGroup, hitBall, this);
+
+		//  add the snake body
+		for (var i = 1; i < numSnakeSections; i++)
 		{
-			snakeSection[i] = game.add.sprite(400, 300, 'ball');
+			snakeSection[i] = game.add.sprite(100, 300, 'ball');
 			snakeSection[i].anchor.setTo(0.5, 0.5);
 		}
 
-		//  Init snakePath array
+		//  init the snake path array
 		for (var i = 0; i <= numSnakeSections * snakeSpacer; i++)
 		{
-			snakePath[i] = new Phaser.Point(400, 300);
+			snakePath[i] = new Phaser.Point(100, 300);
 		}
 
+		// init the body movement (trails after the head)
 		bodyMover = game.time.create(false);
 		bodyMover.loop(Phaser.Timer.SECOND/25, moveSnake, this);
 		bodyMover.start();
 
+		// init keyboard cursors
+		cursors = game.input.keyboard.createCursorKeys();
+
+		// init the pause button using spacebar
 		pauseButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
 
 		pauseButton.onDown.add(pauseGame, game);
+
+		// start the snake
+		game.time.events.add(Phaser.Timer.SECOND, startSnake, this);
 	}
 
 	function update() {
+		// reset the velocity of the snake on keypress
+		snakeHead.body.setZeroVelocity();
+
+		if (isPaused) {
+			return false;
+		}
 
 		if (cursors.up.isDown) {
-			snakeHead.body.setZeroVelocity();
 			snakeHead.body.moveUp(50);
 		} else if (cursors.down.isDown) {
-			snakeHead.body.setZeroVelocity();
 			snakeHead.body.moveDown(50);
 		} else if (cursors.left.isDown) {
-			snakeHead.body.setZeroVelocity();
 			snakeHead.body.moveLeft(50);
 		} else if (cursors.right.isDown) {
-			snakeHead.body.setZeroVelocity();
 			snakeHead.body.moveRight(50);
 		}
+	}
+
+	function startSnake() {
+		console.log("snake should start startSnake");
+		snakeHead.body.moveDown(400);
+	}
+
+	function hitBall() {
+		console.log("hit the ball");
 	}
 
 	function pauseGame() {
@@ -126,12 +128,11 @@ window.onload = function() {
 		// and knock the last position off the end
 
 		var part = snakePath.pop();
-
 		part.setTo(snakeHead.x, snakeHead.y);
 
 		snakePath.unshift(part);
 
-		for (var i = 1; i <= numSnakeSections - 1; i++)
+		for (var i = 1; i < numSnakeSections; i++)
 		{
 			snakeSection[i].x = (snakePath[i * snakeSpacer]).x;
 			snakeSection[i].y = (snakePath[i * snakeSpacer]).y;
@@ -141,6 +142,10 @@ window.onload = function() {
 	function render() {
 
 		game.debug.spriteInfo(snakeHead, 32, 32);
+
+	}
+
+	function buildSnake() {
 
 	}
 };
